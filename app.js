886e695276c242f0f713d240d257e7cc2c3a55f1
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2025-02-01 04:21:23 -0800

    logic finished

---
+++ app.js
@@ -0,0 +1,18 @@
+const numbers = [...document.querySelectorAll(".number")];
+
+const updateCount = (element) => {
+  const value = parseInt(element.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+
+  const updateText = setInterval(() => {
+    initialValue += increment;
+    if (value < initialValue) {
+      clearInterval(updateText);
+      element.textContent = `${value}+`;
+    }
+    element.textContent = `${initialValue}+`;
+  }, 1);
+};
+
+numbers.forEach((number) => updateCount(number));
